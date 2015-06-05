9166b8a147bd4015782427f9a457c5d43f743dd8
Stephen Ryan <stepryan@gmail.com>
2015-06-05 17:38:52 +0100

    Added additional logging messages to loggin listerner

---
+++ jfruit-test-core/src/main/java/logging/BaseTestLogger.java
@@ -0,0 +1,47 @@
+package logging;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+/*
+ * TestLogger to log to test execution status 
+ */
+public class BaseTestLogger implements ITestListener {
+	
+	
+	//Get a log4j logger
+	static Logger logger = LogManager.getLogger(BaseTestLogger.class.getName());
+	
+   public void onTestStart(ITestResult result) {
+		logger.info("TEST START" + result);
+	}
+	
+	public void onTestSuccess(ITestResult result) {
+		logger.info("TEST PASS" + result);		
+	}
+
+	public void onTestFailure(ITestResult result) {
+		logger.error("TEST FAIL: " + result);
+	}
+
+	public void onTestSkipped(ITestResult result) {
+		logger.warn("TEST SKIPPED" + result);		
+	}
+
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		logger.warn("TEST FAILED WITHIN %" + result);		
+		
+	}
+
+	public void onStart(ITestContext context) {
+		logger.info("START SUITE");
+	}
+
+	public void onFinish(ITestContext context) {
+		logger.info("FINISH SUITE");		
+	}
+}
--- jfruit-test-core/src/test/java/coretest/CoreTestCase.java
+++ jfruit-test-core/src/test/java/coretest/CoreTestCase.java
@@ -2,6 +2,7 @@ package coretest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import basetest.BaseTestCase;
@@ -9,14 +10,22 @@ import basetest.BaseTestCase;
 
 @Test
 public class CoreTestCase extends BaseTestCase{
-	
+
 	//Create a logger to append logs to file or console 
-	 static Logger logger = LogManager.getLogger(CoreTestCase.class.getName());
+	static Logger logger = LogManager.getLogger(CoreTestCase.class.getName());
+
+	@Test
+	public void loggingTestPass(){
+		logger.info(": Entering pass slogging test");
+
+		Assert.assertTrue(true);
+	}
 	
 	@Test
-	public void loggingTest(){
-		logger.info(": Entering logging test");
-		
+	public void loggingTestFail(){
+		logger.info(": Entering fail logging test");
+
+		Assert.assertTrue(false);
 	}
 
 }
